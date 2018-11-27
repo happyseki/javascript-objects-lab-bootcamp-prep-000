e2b05696c4b09f3f1e2019794d5a937a8e39719d
Elaina <elainaseki@gmail.com>
2018-11-27 23:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+var recipes = {};
 var obj = { prop: 1 }
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},obj,{[key]:value})
 }
-var recipes = {};
+
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
